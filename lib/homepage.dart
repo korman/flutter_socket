dbdf953ff096c9e8482a553ab4609005a85761f8
korman <korman@qq.com>
2022-10-12 20:46:37 +0800

    reconsturct

---
+++ lib/homepage.dart
@@ -0,0 +1,167 @@
+import 'package:flutter_socket/pb/sc_logic.pb.dart';
+import 'package:flutter_socket/table_item.dart';
+import 'package:flutter_socket/rename_dialog.dart';
+import 'rename_dialog.dart';
+import 'package:flutter_socket/pb/global_define.pb.dart';
+import 'package:flutter_socket/pb/conn.pb.dart';
+import 'package:flutter_socket/pb/cs_logic.pb.dart';
+import 'package:flutter/material.dart';
+import 'network.dart';
+
+class Homepage extends StatefulWidget {
+  Homepage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+  List<TableRow> _tables = [];
+  MapInfo _mapInfo = MapInfo(width: 0, height: 0);
+  List<Node> _nodes = [];
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  Table _table = Table();
+  TextEditingController _textController = TextEditingController(text: '');
+
+  @override
+  void initState() {
+    NetworkManager.getInstance().registerMsgHandler(1, (byteData) {
+      var sayReq = SayReq.fromBuffer(byteData);
+      print(sayReq.text);
+
+      SayReq req = SayReq(text: 'Yes!Im Alive.');
+      var writeBuffer = req.writeToBuffer();
+
+      if (!NetworkManager.getInstance().sendMsgToServer(1, writeBuffer)) {
+        print("发送失败");
+      }
+    });
+
+    NetworkManager.getInstance().registerMsgHandler(2, (byteData) {
+      RegisterReply reply = RegisterReply.fromBuffer(byteData);
+      if (reply.result == RegisterResult.REG_SUCCEEDED) {
+        print("注册成功");
+      }
+    });
+
+    NetworkManager.getInstance().registerMsgHandler(3, (byteData) {
+      LockNodeReply reply = LockNodeReply.fromBuffer(byteData);
+      if (reply.result == LockResult.LOCK_SUCCEEDED) {
+        print("锁定成功");
+      }
+    });
+
+    NetworkManager.getInstance().registerMsgHandler(11, (byteData) {
+      InitMapInfo initInfo = InitMapInfo.fromBuffer(byteData);
+      widget._mapInfo = initInfo.info;
+
+      for (int i = 0; i < widget._mapInfo.width * widget._mapInfo.height; i++) {
+        int y = i % widget._mapInfo.width;
+        int x = i - y * widget._mapInfo.width;
+        Node node = Node(x: x, y: y, text: '', state: LockStatus.COMMON_NODE);
+
+        widget._nodes.add(node);
+      }
+      setState(() {
+        updateTables();
+      });
+    });
+
+    NetworkManager.getInstance().registerMsgHandler(12, (byteData) {
+      UpdateNodes updateNodes = UpdateNodes.fromBuffer(byteData);
+
+      widget._nodes = updateNodes.nodes;
+
+      setState(() {
+        updateTables();
+      });
+    });
+
+    super.initState();
+  }
+
+  void updateTables() {
+    _table = Table(
+      border: TableBorder.all(),
+      defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+      defaultColumnWidth: const FixedColumnWidth(125),
+      children: widget._tables,
+    );
+
+    widget._tables.clear();
+
+    if (widget._mapInfo.width == 0 || widget._mapInfo.height == 0) {
+      return;
+    }
+    for (int yy = 0; yy < widget._mapInfo.height; yy++) {
+      List<Widget> children = [];
+      for (int xx = 0; xx < widget._mapInfo.width; xx++) {
+        var text = widget._nodes[yy * widget._mapInfo.width + xx].text;
+        var color = Colors.white;
+
+        if (widget._nodes[yy * widget._mapInfo.width + xx].state ==
+            LockStatus.LOCKED_NODE) {
+          color = Colors.black;
+        }
+
+        Widget w = buildItem(text, color, xx, yy);
+        children.add(w);
+      }
+
+      TableRow row = TableRow(children: children);
+
+      widget._tables.add(row);
+    }
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      showDialog(
+          barrierDismissible: false,
+          context: context,
+          builder: (context) {
+            return RenameDialog(
+              contentWidget: RenameDialogContent(
+                title: "请输入新的家庭名称",
+                okBtnTap: () {
+                  print(
+                    "输入框中的文字为:${_textController.text}",
+                  );
+
+                  RegisterReq req =
+                      RegisterReq(name: _textController.text, pass: '123456');
+                  var writeBuffer = req.writeToBuffer();
+
+                  if (!NetworkManager.getInstance()
+                      .sendMsgToServer(2, writeBuffer)) {
+                    print("发送失败");
+                  }
+                },
+                vc: _textController,
+                cancelBtnTap: () {},
+              ),
+            );
+          });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: _table,
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.person_add),
+      ),
+    );
+  }
+
+  Widget buildItem(String content, Color backgroundColor, int x, int y) {
+    return TableItem(color: backgroundColor, text: content, x: x, y: y);
+  }
+}
--- lib/rename_dialog.dart
+++ lib/rename_dialog.dart
@@ -1,116 +1,12 @@
 import 'package:flutter/material.dart';
 
-double btnHeight = 60;
-double borderWidth = 2;
-
-class RenameDialogContent extends StatefulWidget {
-  String title;
-  String cancelBtnTitle;
-  String okBtnTitle;
-  VoidCallback cancelBtnTap;
-  VoidCallback okBtnTap;
-  TextEditingController vc;
-  RenameDialogContent(
-      {super.key,
-      required this.title,
-      this.cancelBtnTitle = "取消",
-      this.okBtnTitle = "确认",
-      required this.cancelBtnTap,
-      required this.okBtnTap,
-      required this.vc});
-
-  @override
-  State<RenameDialogContent> createState() => _RenameDialogContentState();
-}
-
-class _RenameDialogContentState extends State<RenameDialogContent> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print("build");
-
-    return Container(
-        margin: const EdgeInsets.only(top: 20),
-        height: 200,
-        width: 10000,
-        alignment: Alignment.bottomCenter,
-        child: Column(
-          children: [
-            Container(
-                alignment: Alignment.center,
-                child: Text(
-                  widget.title,
-                  style: const TextStyle(color: Colors.grey),
-                )),
-            const Spacer(),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-              child: TextField(
-                style: const TextStyle(color: Colors.black87),
-                controller: widget.vc,
-                decoration: const InputDecoration(
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue),
-                    )),
-              ),
-            ),
-            Container(
-              // color: Colors.red,
-              height: btnHeight,
-              margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-              child: Column(
-                children: [
-                  Container(
-                    // 按钮上面的横线
-                    width: double.infinity,
-                    color: Colors.blue,
-                    height: borderWidth,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      TextButton(
-                        onPressed: () {
-                          widget.vc.text = "";
-                          widget.cancelBtnTap();
-                          Navigator.of(context).pop();
-                        },
-                        child: Text(
-                          widget.cancelBtnTitle,
-                          style:
-                              const TextStyle(fontSize: 22, color: Colors.blue),
-                        ),
-                      ),
-                      Container(
-                        // 按钮中间的竖线
-                        width: borderWidth,
-                        color: Colors.blue,
-                        height: btnHeight - borderWidth - borderWidth,
-                      ),
-                      TextButton(
-                          onPressed: () {
-                            widget.okBtnTap();
-                            Navigator.of(context).pop();
-                            widget.vc.text = "";
-                          },
-                          child: Text(
-                            widget.okBtnTitle,
-                            style: const TextStyle(
-                                fontSize: 22, color: Colors.blue),
-                          )),
-                    ],
-                  ),
-                ],
-              ),
-            )
-          ],
-        ));
-  }
+class RenameDialog extends AlertDialog {
+  RenameDialog({super.key, required Widget contentWidget})
+      : super(
+          content: contentWidget,
+          contentPadding: EdgeInsets.zero,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+              side: const BorderSide(color: Colors.blue, width: 3)),
+        );
 }
